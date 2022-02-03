1f8e73147e936565fa896ff2d8d7461c0455943a
Chunlee Thong <chunlee.thong@gmail.com>
2022-02-03 20:55:50 +0700

    update

---
+++ assets/generator.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+File resourceClass = File("../lib/src/constant/app_assets.dart");
+
+void main() {
+  generateFile();
+}
+
+void generateFile() async {
+  Directory dir = Directory('./images');
+  var buffer = StringBuffer();
+  buffer.writeln("//this class is generated from assets/generator.dart");
+  buffer.write("class AppAssets {");
+  List<FileSystemEntity> filesSystem = dir.listSync(recursive: true);
+
+  for (final fileSystem in filesSystem) {
+    if (fileSystem is Directory) {
+      continue;
+    } else {
+      buffer.write(generateImageAssetsClass(fileSystem.uri.path));
+    }
+    await resourceClass.writeAsString(buffer.toString());
+  }
+  resourceClass.writeAsStringSync("\n}", mode: FileMode.append);
+}
+
+String generateImageAssetsClass(String path) {
+  final String imageName = path.split("/").last.replaceAll("%20", " ");
+  final String imagePath = path.replaceAll("%20", " ");
+  String imageFieldName = imageName.replaceAll(RegExp(r'[-\s+\b|\b\s]'), " ").split(".").first;
+  final List<String> words = imageFieldName.split(" ").map(_upperCaseFirstLetter).toList();
+  if (words.isNotEmpty) {
+    words[0] = words[0].toLowerCase();
+  }
+  imageFieldName = words.join("");
+  return '\n  static const String $imageFieldName = "assets/$imagePath";';
+}
+
+String _upperCaseFirstLetter(String word) {
+  return '${word.substring(0, 1).toUpperCase()}${word.substring(1).toLowerCase()}';
+}
---
+++ lib/src/constant/app_assets.dart
@@ -0,0 +1,4 @@
+//this class is generated from assets/generator.dart
+class AppAssets {
+  static const String flutterLogo = "assets/images/flutter-logo.png";
+}
