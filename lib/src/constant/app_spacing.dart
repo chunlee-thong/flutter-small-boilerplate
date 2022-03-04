9b85f7c80dd8c1f2715ed4feac31ae903b9e7db9
Chunlee Thong <chunlee.thong@gmail.com>
2022-03-04 20:44:35 +0700

    Update app_spacing.dart

--- lib/src/constant/app_spacing.dart
+++ lib/src/constant/app_spacing.dart
@@ -4,7 +4,9 @@ import 'package:sura_flutter/sura_flutter.dart';
 class AppSpacing {
   static const EdgeInsets pageSpacing = EdgeInsets.all(16);
 
-  ///Responsive value for 16
+  ///Responsive value for 8
   static final double value8 = SuraResponsive.value(8, 14, 20);
+
+  ///Responsive value for 16
   static final double value16 = SuraResponsive.value(16, 22, 28);
 }
